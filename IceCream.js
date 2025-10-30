dc7aaaf6126a4e372b3cb84e1968ddcacf82f0c9
rohan <rairohan14303@gmail.com>
2025-10-30 21:25:23 +0545

    callback hell

--- IceCream.js
+++ IceCream.js
@@ -6,14 +6,36 @@ let stocks = {//objects created as stocks
 }
 
 
-let order = (Fruit_name,call_production) => { //arrow fuction which takes arguments
+let order = (Fruit_num,call_production) => { //arrow fuction which takes arguments
     setTimeout(()=>{
-        console.log(`${stocks.Fruits[Fruit_name]}`)//the fruit_name hold a value later used here which acts as a index num
+        console.log(`${stocks.Fruits[Fruit_num]} has been selected `)//the fruit_num hold a value later used here which acts as a index num
         call_production()//callback
     },2000)//delay of 2 secs
   
 }
 let production = () => {
-   console.log("Production has started!")
+    setTimeout(()=>
+    {
+         console.log("Production has started!")
+         setTimeout(()=>{
+             console.log("Fruit have been chopped")
+             setTimeout(()=>{
+                console.log(`${stocks.Liquid[0]} and ${stocks.Liquid[1]} were selected`)
+                setTimeout(()=>{
+                    console.log(`Machine has started !`)
+                    setTimeout(()=>{
+                        console.log(`${stocks.Holder[0]} was selected`)
+                        setTimeout(()=>{
+                            console.log(`${stocks.Toppings[1]} was selected`)   
+                            setTimeout(()=>{
+                                console.log("Ice Cream was served")
+                            },2000)
+                        },3000)
+                    },2000)
+                },1000)
+             },1000)
+         },2000)          
+    },1000)
+  
 }
 order(0,production)//calling order fucntion
