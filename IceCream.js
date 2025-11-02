4c32df5736b9f662d58fc614e014d5c6849f5122
rohan <rairohan14303@gmail.com>
2025-11-02 22:31:32 +0545

    resolve and reject

--- IceCream.js
+++ IceCream.js
@@ -4,38 +4,25 @@ let stocks = {//objects created as stocks
     Holder:["Cone","Cup","Sticks"],
     Toppings:["Chocolates","Sprinkles"]
 }
-
-
-let order = (Fruit_num,call_production) => { //arrow fuction which takes arguments
-    setTimeout(()=>{
-        console.log(`${stocks.Fruits[Fruit_num]} has been selected `)//the fruit_num hold a value later used here which acts as a index num
-        call_production()//callback
-    },2000)//delay of 2 secs
-  
+let is_open = true;
+let order = (time,work)=>{
+    return new Promise( (resolve,reject)=>{
+        if(is_open){
+            setTimeout(()=>{
+                resolve( work())
+            },time)            
+        }
+        else{
+            reject(console.log(`Sorry the shop is closed!`))
+        }
+})
 }
-let production = () => {
-    setTimeout(()=>
-    {
-         console.log("Production has started!")
-         setTimeout(()=>{
-             console.log("Fruit have been chopped")
-             setTimeout(()=>{
-                console.log(`${stocks.Liquid[0]} and ${stocks.Liquid[1]} were selected`)
-                setTimeout(()=>{
-                    console.log(`Machine has started !`)
-                    setTimeout(()=>{
-                        console.log(`${stocks.Holder[0]} was selected`)
-                        setTimeout(()=>{
-                            console.log(`${stocks.Toppings[1]} was selected`)   
-                            setTimeout(()=>{
-                                console.log("Ice Cream was served")
-                            },2000)
-                        },3000)
-                    },2000)
-                },1000)
-             },1000)
-         },2000)          
-    },1000)
-  
-}
-order(0,production)//calling order fucntion
+order(2000, ()=>console.log(`${stocks.Fruits[0]} was selected `))
+
+.then(()=> {return order(1000,()=>console.log("Prodcution has stared"))})
+.then(()=>{ return order(2000,()=>console.log(`${stocks.Fruits[0]} is being cut`))})
+.then(()=>{ return order(2000, ()=>console.log(`${stocks.Liquid[0]} was selected`))})
+.then(()=>{ return order(1000,()=>console.log("Machine has been started"))})
+.then(()=>{return order(2000,()=>console.log(`${stocks.Holder[1]} was taken`))})
+.then(()=>{return order(3000,()=>console.log(`${stocks.Toppings[0]} was selected`))})
+.then(()=>{return order(2000,()=>console.log(`Ice-cream is being served`))})
